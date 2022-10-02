c0c7128bb7786d44693af12c4a647df4ae72b848
ghedjati oussama <36881523+Ghadje@users.noreply.github.com>
2022-10-02 23:05:37 +0100

    [ADD]: new theme

--- src/theme/themes.ts
+++ src/theme/themes.ts
@@ -176,3 +176,92 @@ export const light: SaleorThemeColors = {
   paperBorder: "#EAEAEA",
   theme: "light",
 };
+
+export const purple: SaleorThemeColors = {
+  alert: {
+    paper: {
+      error: "#FFD6D9",
+      info: "#FFFFFF",
+      success: "#DFF3E9",
+      warning: "#FFF4E4",
+    },
+    icon: {
+      error: "#FE6E76",
+      info: "#28234A",
+      success: "#5DC292",
+      warning: "#FFB84E",
+    },
+  },
+  highlightInactive: {
+    default: "#C8C8C8",
+  },
+  autofill: "#f4f6c5",
+  background: {
+    default: "#EFF5F8",
+    paper: "#FFFFFF",
+  },
+  checkbox: {
+    default: "#616161",
+  },
+  divider: "#EAEAEA",
+  gray: {
+    default: "#C8C8C8",
+    disabled: "#C0CFE2",
+  },
+
+  active: {
+    1: "#056DFF",
+    2: "#378AFF",
+    3: "#68A7FF",
+    4: "#C1DBFF",
+    5: "#E6F0FF",
+  },
+  main: {
+    1: "#28234A",
+    2: "rgba(40, 35, 74, 0.8)",
+    3: "rgba(40, 35, 74, 0.6)",
+    4: "rgba(40, 35, 74, 0.4)",
+    5: "rgba(40, 35, 74, 0.1)",
+  },
+  fail: {
+    verydark: "#9F2E2E",
+    dark: "#B65757",
+    mid: "#FEADAD",
+    light: "#FEDEDE",
+  },
+  warning: {
+    verydark: "#97661F",
+    dark: "#B78A48",
+    mid: "#FFE6AB",
+    light: "#FFF4D2",
+  },
+  success: {
+    verydark: "#22805D",
+    dark: "#439C7B",
+    mid: "#A4E8BA",
+    light: "#E6F9F0",
+  },
+  info: {
+    verydark: "#004697",
+    dark: "#105CB3",
+    mid: "#99E6FD",
+    light: "#D2F6FE",
+  },
+  errorAction: {
+    1: "#B63755",
+    2: "#D36474",
+    3: "#E9878B",
+    4: "#F7B6B2",
+    5: "#FBDDD8",
+  },
+  generic: {
+    light: "#F1F1F1",
+    mid: "#EAEAEA",
+    dark: "#7C7F7F",
+    verydark: "#252929",
+  },
+
+  disabled: "#C2D1E4",
+  paperBorder: "#EAEAEA",
+  theme: "light",
+};

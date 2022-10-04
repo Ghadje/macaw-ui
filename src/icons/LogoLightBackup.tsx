607b5eee0b068fa9992b9dab846196b306c72cee
ghedjati oussama <36881523+Ghadje@users.noreply.github.com>
2022-10-04 21:28:23 +0100

    [UDP]: update the logos

--- src/icons/LogoLightBackup.tsx
+++ src/icons/LogoLightBackup.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export const LogoDark: React.FC = () => (
+export const LogoLight: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg 
   width="36"
   height="33"
+  {...props}
   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 195.98 212.22"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#6920f0;isolation:isolate;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path className="cls-1" d="M107.14,212.22a143.85,143.85,0,0,1-40.91-6.31c-27.7-8.2-54.93-41-63.35-76.24-8.82-37,2.75-72.81,32.58-101C52.86,12.29,72.5,2.81,93.83.53c16.84-1.79,34.43,1,52.26,8.19,29,11.75,49,31.63,49.89,32.47L170.56,66.68C168.11,64.26,110,7.88,60.17,54.89,39.52,74.37,32,96.72,37.9,121.31c6.39,26.79,26.85,46.62,38.55,50.08,33,9.77,59.58,4.85,83.1-15.54V138.21H108.41v-36h87.14v69l-5.35,5.28C166.11,200.28,138.29,212.22,107.14,212.22Z"/><circle className="cls-2" cx="170.98" cy="42.74" r="6.11"/><circle className="cls-2" cx="147.81" cy="26.7" r="6.11"/><circle className="cls-2" cx="120.25" cy="19.57" r="6.11"/><circle className="cls-2" cx="92.09" cy="20.76" r="6.11"/><circle className="cls-2" cx="66.24" cy="31.02" r="6.11"/><circle className="cls-2" cx="44.37" cy="49.13" r="6.11"/><circle className="cls-2" cx="29.12" cy="72.9" r="6.11"/><circle className="cls-2" cx="22.46" cy="99.65" r="6.11"/><circle className="cls-2" cx="25.27" cy="128.69" r="6.11"/><circle className="cls-2" cx="36.81" cy="154.06" r="6.11"/><circle className="cls-2" cx="54.91" cy="175.16" r="6.11"/><circle className="cls-2" cx="79.76" cy="189.04" r="6.11"/><circle className="cls-2" cx="107.7" cy="195.22" r="6.11"/><circle className="cls-2" cx="135.83" cy="191.42" r="6.11"/><circle className="cls-2" cx="160.54" cy="179.25" r="6.11"/><circle className="cls-2" cx="176.7" cy="152.35" r="6.11"/><circle className="cls-2" cx="177.18" cy="121.46" r="6.11"/><circle className="cls-2" cx="150.75" cy="121.46" r="6.11"/><circle className="cls-2" cx="125.56" cy="121.22" r="6.11"/></g></g></svg>
 );
 
-LogoDark.displayName = "LogoDark";
+LogoLight.displayName = "LogoLight";

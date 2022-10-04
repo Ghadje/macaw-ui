88ada221e0fb4c161054bc68bc77c7d593399a12
ghedjati oussama <36881523+Ghadje@users.noreply.github.com>
2022-10-04 22:01:50 +0100

    [FIX]: fix logo and update version

--- src/icons/LogoLight.tsx
+++ src/icons/LogoLight.tsx
@@ -1,27 +1,11 @@
 import React from "react";
 
 export const LogoLight: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg
-    width="36"
-    height="33"
-    viewBox="0 0 36 33"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M19.7838 0L13.4043 6.64709H29.1638L35.5433 0H19.7838Z"
-      fill="#8AC4EB"
-    />
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M6.59759 2.13062C6.4332 2.13062 6.27552 2.1958 6.15911 2.31187L0.182532 8.27133C0.00456112 8.44879 -0.0488948 8.71603 0.0471181 8.94829C0.143131 9.18056 0.369679 9.33205 0.621008 9.33205H21.3482C21.5126 9.33205 21.6703 9.26687 21.7867 9.1508L27.7632 3.19134C27.9412 3.01388 27.9947 2.74664 27.8987 2.51437C27.8026 2.28211 27.5761 2.13062 27.3248 2.13062H6.59759ZM2.12325 8.09007L6.85429 3.37259H25.8225L21.0915 8.09007H2.12325ZM23.9789 27.3629C23.9789 30.6214 22.4977 33 16.6072 33C11.4908 33 9.43754 31.599 9 28.1124L13.6451 27.4281C13.9144 29.1551 14.5539 29.5135 16.6745 29.5135C18.5933 29.5135 19.2328 29.0573 19.2328 28.0146C19.2328 26.7437 18.492 26.418 16.5735 26.0594C16.4207 26.0337 16.2653 26.0082 16.1079 25.9823C13.0752 25.4835 9.30464 24.8634 9.33654 20.6504C9.33654 17.2293 11.2216 15.0787 16.5735 15.0787C21.4542 15.0787 23.3055 16.7404 23.7097 20.0314L18.9298 20.7482C18.7614 19.2168 18.2566 18.6954 16.4389 18.6954C14.9577 18.6954 14.0489 19.0867 14.0489 20.1292C14.0489 21.5304 15.0924 21.7258 17.3814 22.1167C20.3772 22.6056 23.9789 23.3549 23.9789 27.3629Z"
-      fill="#FAFAFA"
-    />
-  </svg>
+  <svg 
+  width="36"
+  height="33"
+  {...props}
+  id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 195.98 212.22"><path d="M156.57,253.52a144.2,144.2,0,0,1-40.92-6.3C88,239,60.72,206.24,52.31,171,43.49,134,55.05,98.16,84.88,70c17.4-16.42,37-25.9,58.38-28.17,16.84-1.8,34.42,1,52.25,8.19,29,11.75,49.06,31.63,49.9,32.47L220,108c-2.45-2.43-60.59-58.8-110.4-11.79C89,115.68,81.45,138,87.32,162.62c6.4,26.79,26.85,46.61,38.55,50.08,33,9.77,59.59,4.84,83.11-15.54V179.52H157.83v-36H245v69l-5.36,5.29C215.53,241.59,187.71,253.52,156.57,253.52Z" transform="translate(-49.42 -41.31)" fill="#fff" style={{isolation:"isolate"}}/><circle cx="170.98" cy="42.74" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="147.81" cy="26.7" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="120.25" cy="19.57" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="92.09" cy="20.76" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="66.24" cy="31.02" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="44.37" cy="49.13" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="29.12" cy="72.9" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="22.46" cy="99.65" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="25.27" cy="128.69" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="36.81" cy="154.06" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="54.91" cy="175.16" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="79.76" cy="189.04" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="107.7" cy="195.22" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="135.83" cy="191.42" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="160.54" cy="179.25" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="176.7" cy="152.35" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="177.18" cy="121.46" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="150.75" cy="121.46" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/><circle cx="125.56" cy="121.22" r="6.11" fill="#6922ef" style={{isolation:"isolate"}}/></svg>
 );
 
 LogoLight.displayName = "LogoLight";
